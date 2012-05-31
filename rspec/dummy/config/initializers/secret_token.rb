a2c32f4e85c7ecfc161f07493bc2aea50aabb416
Carlos David Gonzalez Abraham <carlosdavid@gonzalez-abraham.com.mx>
2012-05-31 13:44:18 -0500

    Removed rspec directory.

--- rspec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '99a34c11dfb855c3543bc4a33e35bd8ffd3b6a09a4ed6a0e53bb8aac06293e275547ad3978b010125a3a6e3502794743af6bda7ded1d12d2db4dba2e1eb0edf6'
